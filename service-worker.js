29be63a5f27cbee238c88dab75da82d67ec10990
Misha030220 <peter.schmidt87@gmx.de>
2024-09-21 14:25:40 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,27 @@
 const CACHE_NAME = 'my-cache-v1';
 const urlsToCache = [
-  '/index.html',  // Stelle sicher, dass der Pfad absolut ist
-  '/sketch.js',
-  '/manifest.json',
+    '/TheColorGame/index.html',
+    '/TheColorGame/sketch.js',
+    '/TheColorGame/manifest.json',
+    '/TheColorGame/style.css' // Füge diese Zeile hinzu, wenn du die CSS-Datei auch cachen möchtest
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Caching all resources');
-        return cache.addAll(urlsToCache);
-      })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
 });
 
 
